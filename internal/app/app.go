9edcc308ce83a39895d0ad53dca7a73c479bbe9b
Duc Nguyen <minhduc140583@gmail.com>
2023-09-17 23:55:36 +0700

    Apply new version

--- internal/app/app.go
+++ internal/app/app.go
@@ -37,7 +37,10 @@ func NewApp(ctx context.Context, cfg Config) (*ApplicationContext, error) {
 	}
 
 	logError := log.LogError
-	validator := v.NewValidator()
+	validator, err := v.NewValidator()
+	if err != nil {
+		return nil, err
+	}
 
 	userType := reflect.TypeOf(model.User{})
 	userQuery := query.NewBuilder(userType)
